9a1ed0c533d345dce93bfe748f624540748b8705
Mikian Musser <Bobar312@gmail.com>
2018-10-23 15:43:03 -0700

    Added files

---
+++ class/Day 1/code/sketch.js
@@ -0,0 +1,13 @@
+// Mikian Musser
+// p5.js Sprint
+// Code Central
+// 10.23.18
+// https://p5js.org/reference/
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(151);
+}
